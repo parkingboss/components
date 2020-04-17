6b06df3c4a7383b8803e6d5bedf3ba0e8d61303a
Chris Pfohl <christopher.pfohl@gmail.com>
2020-04-17 12:31:24 -0400

    Add Updated component

---
+++ src/Updated.svelte
@@ -0,0 +1,14 @@
+<script>
+    const formatter = new Intl.DateTimeFormat("en-US", {
+        year:undefined,
+        month:"short",
+        weekday:"short",
+        day:"numeric",
+        hour:"numeric",
+        minute:"numeric",
+        timeZoneName:"short",
+    });
+    export let time;
+</script>
+
+<time class="updated" datetime="{time.toISOString()}">{formatter.format(time)}</time>
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ export { default as Address } from './Address.svelte';
 export { default as Contact } from './Contact.svelte';
 export { default as File } from './File.svelte';
 export { default as Image } from './Image.svelte';
-export { default as PropertySummary } from './PropertySummary.svelte';
+export { default as Updated } from './Updated.svelte';
