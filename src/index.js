8538750df1937ecd89b2aad3e63afa409c5b0239
Chris Pfohl <christopher.pfohl@gmail.com>
2020-04-23 15:34:18 -0400

    Make sure Camera options are exported

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ export { default as Image } from './Image.svelte';
 export { default as Updated } from './Updated.svelte';
 export { default as Note } from './Note.svelte';
 export { default as Timestamp } from './Timestamp.svelte';
-export { default as Camera } from './Camera.svelte';
+export { default as Camera, navigating, setupPageNavigating } from './Camera.svelte';
